56bba0699c2fc4fdcdc629705ebd9ed22ca2e528
Cameron White <cameronwhite4121@gmail.com>
2024-12-02 16:07:11 -0800

    Add an overlay that shows hidden reset button, added 3 player game activity

---
+++ app/src/main/java/com/example/simplelifetracker/FourPlayerGame.kt
@@ -0,0 +1,229 @@
+package com.example.simplelifetracker
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class FourPlayerGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_four_player_game)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        gameStart()
+    }
+    private fun gameStart() {
+
+        // Initialize buttons/textviews
+        val winningPlayer: TextView = findViewById(R.id.winningPlayer)
+        val resetButton: Button = findViewById(R.id.resetButton)
+        val settingsButton: Button = findViewById(R.id.settingsButton)
+        val closeOverlayButton: Button = findViewById(R.id.closeOverlayButton)
+
+        // Subtract life buttons
+        val player1Minus: Button = findViewById(R.id.player1minus)
+        val player2Minus: Button = findViewById(R.id.player2minus)
+        val player3Minus: Button = findViewById(R.id.player3minus)
+        val player4Minus: Button = findViewById(R.id.player4minus)
+
+        // Add life buttons
+        val player1Plus: Button = findViewById(R.id.player1plus)
+        val player2Plus: Button = findViewById(R.id.player2plus)
+        val player3Plus: Button = findViewById(R.id.player3plus)
+        val player4Plus: Button = findViewById(R.id.player4plus)
+
+        // Life tracker textviews
+        val player1LifeTextView: TextView = findViewById(R.id.player1life)
+        val player2LifeTextView: TextView = findViewById(R.id.player2life)
+        val player3LifeTextView: TextView = findViewById(R.id.player3life)
+        val player4LifeTextView: TextView = findViewById(R.id.player4life)
+
+        // Initialize hidden overlay
+        val overlayContainer: View = findViewById(R.id.overlayContainer)
+
+        // Set life totals
+        var player1Life = 40
+        player1LifeTextView.text = "$player1Life"
+        var player2Life = 40
+        player2LifeTextView.text = "$player2Life"
+        var player3Life = 40
+        player3LifeTextView.text = "$player3Life"
+        var player4Life = 40
+        player4LifeTextView.text = "$player4Life"
+
+        fun checkForWinner() {
+            // Check for 3 losers, if so, there is a winner
+            var numPlayerRip = 0
+            if (player1Life == 0) {
+                numPlayerRip++
+            }
+            if (player2Life == 0) {
+                numPlayerRip++
+            }
+            if (player3Life == 0) {
+                numPlayerRip++
+            }
+            if (player4Life == 0) {
+                numPlayerRip++
+            }
+            // If there is a winner, display the winner
+            if (numPlayerRip == 3) {
+                if (player1Life != 0) {
+                    winningPlayer.text = "Player 1 wins!"
+                }
+                if (player2Life != 0) {
+                    winningPlayer.text = "Player 2 wins!"
+                }
+                if (player3Life != 0) {
+                    winningPlayer.text = "Player 3 wins!"
+                }
+                if (player4Life != 0) {
+                    winningPlayer.text = "Player 4 wins!"
+                }
+            }
+            else {
+                // Code is necessary to clear winningPlayer textview
+                // if a player accidentally loses the game.
+                winningPlayer.text = ""
+            }
+        }
+
+        fun openOverlay() {
+            overlayContainer.setVisibility(View.VISIBLE)
+
+            // Disable all non-overlay buttons
+            player1Plus.isEnabled = false
+            player2Plus.isEnabled = false
+            player3Plus.isEnabled = false
+            player4Plus.isEnabled = false
+
+            player1Minus.isEnabled = false
+            player2Minus.isEnabled = false
+            player3Minus.isEnabled = false
+            player4Minus.isEnabled = false
+
+            settingsButton.isEnabled = false
+        }
+
+        fun closeOverlay() {
+            overlayContainer.setVisibility(View.GONE)
+
+            // Re-enable all player buttons and others when overlay is hidden
+            player1Plus.isEnabled = true
+            player2Plus.isEnabled = true
+            player3Plus.isEnabled = true
+            player4Plus.isEnabled = true
+
+            player1Minus.isEnabled = true
+            player2Minus.isEnabled = true
+            player3Minus.isEnabled = true
+            player4Minus.isEnabled = true
+
+            settingsButton.isEnabled = true
+        }
+
+        settingsButton.setOnClickListener {
+            openOverlay()
+        }
+
+        closeOverlayButton.setOnClickListener {
+            closeOverlay()
+        }
+
+        resetButton.setOnClickListener {
+            closeOverlay()
+
+            player1Life = 40
+            player1LifeTextView.text = "$player1Life"
+            player2Life = 40
+            player2LifeTextView.text = "$player2Life"
+            player3Life = 40
+            player3LifeTextView.text = "$player3Life"
+            player4Life = 40
+            player4LifeTextView.text = "$player4Life"
+            winningPlayer.text = ""
+        }
+
+        // Subtract life button listeners
+        player1Minus.setOnClickListener {
+            player1Life--
+            if (player1Life <= 0) {
+                player1Life = 0
+                player1LifeTextView.text = "R.I.P"
+                checkForWinner()
+            }
+            else {
+                player1LifeTextView.text = "$player1Life"
+            }
+        }
+        player2Minus.setOnClickListener {
+            player2Life--
+            if (player2Life <= 0) {
+                player2Life = 0
+                player2LifeTextView.text = "R.I.P"
+                checkForWinner()
+            }
+            else {
+                player2LifeTextView.text = "$player2Life"
+            }
+        }
+        player3Minus.setOnClickListener {
+            player3Life--
+            if (player3Life <= 0) {
+                player3Life = 0
+                player3LifeTextView.text = "R.I.P"
+                checkForWinner()
+            }
+            else {
+                player3LifeTextView.text = "$player3Life"
+            }
+        }
+        player4Minus.setOnClickListener {
+            player4Life--
+            if (player4Life <= 0) {
+                player4Life = 0
+                player4LifeTextView.text = "R.I.P"
+                checkForWinner()
+            }
+            else {
+                player4LifeTextView.text = "$player4Life"
+            }
+        }
+
+        // Add life button listeners
+        player1Plus.setOnClickListener {
+            player1Life++
+            player1LifeTextView.text = "$player1Life"
+            // Call check for winner in case we need to clear the winning player textview
+            checkForWinner()
+        }
+        player2Plus.setOnClickListener {
+            player2Life++
+            player2LifeTextView.text = "$player2Life"
+            // Call check for winner in case we need to clear the winning player textview
+            checkForWinner()
+        }
+        player3Plus.setOnClickListener {
+            player3Life++
+            player3LifeTextView.text = "$player3Life"
+            // Call check for winner in case we need to clear the winning player textview
+            checkForWinner()
+        }
+        player4Plus.setOnClickListener {
+            player4Life++
+            player4LifeTextView.text = "$player4Life"
+            // Call check for winner in case we need to clear the winning player textview
+            checkForWinner()
+        }
+    }
+}
--- app/src/main/java/com/example/simplelifetracker/ThreePlayerGame.kt
+++ app/src/main/java/com/example/simplelifetracker/ThreePlayerGame.kt
@@ -8,11 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ThreePlayerGame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_three_player_game)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,27 +23,24 @@ class MainActivity : AppCompatActivity() {
     private fun gameStart() {
 
         // Initialize buttons/textviews
-
         val winningPlayer: TextView = findViewById(R.id.winningPlayer)
         val resetButton: Button = findViewById(R.id.resetButton)
+        val settingsButton: Button = findViewById(R.id.button_settings);
 
         // Subtract life buttons
         val player1Minus: Button = findViewById(R.id.player1minus)
         val player2Minus: Button = findViewById(R.id.player2minus)
         val player3Minus: Button = findViewById(R.id.player3minus)
-        val player4Minus: Button = findViewById(R.id.player4minus)
 
         // Add life buttons
         val player1Plus: Button = findViewById(R.id.player1plus)
         val player2Plus: Button = findViewById(R.id.player2plus)
         val player3Plus: Button = findViewById(R.id.player3plus)
-        val player4Plus: Button = findViewById(R.id.player4plus)
 
         // Life tracker textviews
         val player1LifeTextView: TextView = findViewById(R.id.player1life)
         val player2LifeTextView: TextView = findViewById(R.id.player2life)
         val player3LifeTextView: TextView = findViewById(R.id.player3life)
-        val player4LifeTextView: TextView = findViewById(R.id.player4life)
 
         // Set life totals
         var player1Life = 40
@@ -52,8 +49,10 @@ class MainActivity : AppCompatActivity() {
         player2LifeTextView.text = "$player2Life"
         var player3Life = 40
         player3LifeTextView.text = "$player3Life"
-        var player4Life = 40
-        player4LifeTextView.text = "$player4Life"
+
+        settingsButton.setOnClickListener {
+
+        }
 
         fun checkForWinner() {
             // Check for 3 losers, if so, there is a winner
@@ -67,11 +66,9 @@ class MainActivity : AppCompatActivity() {
             if (player3Life == 0) {
                 numPlayerRip++
             }
-            if (player4Life == 0) {
-                numPlayerRip++
-            }
+
             // If there is a winner, display the winner
-            if (numPlayerRip == 3) {
+            if (numPlayerRip == 2) {
                 if (player1Life != 0) {
                     winningPlayer.text = "Player 1 wins!"
                 }
@@ -80,9 +77,6 @@ class MainActivity : AppCompatActivity() {
                 }
                 if (player3Life != 0) {
                     winningPlayer.text = "Player 3 wins!"
-                }
-                if (player4Life != 0) {
-                    winningPlayer.text = "Player 4 wins!"
                 }
             }
             else {
@@ -99,8 +93,6 @@ class MainActivity : AppCompatActivity() {
             player2LifeTextView.text = "$player2Life"
             player3Life = 40
             player3LifeTextView.text = "$player3Life"
-            player4Life = 40
-            player4LifeTextView.text = "$player4Life"
             winningPlayer.text = ""
         }
 
@@ -138,17 +130,6 @@ class MainActivity : AppCompatActivity() {
                 player3LifeTextView.text = "$player3Life"
             }
         }
-        player4Minus.setOnClickListener {
-            player4Life--
-            if (player4Life <= 0) {
-                player4Life = 0
-                player4LifeTextView.text = "R.I.P"
-                checkForWinner()
-            }
-            else {
-                player4LifeTextView.text = "$player4Life"
-            }
-        }
 
         // Add life button listeners
         player1Plus.setOnClickListener {
@@ -166,12 +147,6 @@ class MainActivity : AppCompatActivity() {
         player3Plus.setOnClickListener {
             player3Life++
             player3LifeTextView.text = "$player3Life"
-            // Call check for winner in case we need to clear the winning player textview
-            checkForWinner()
-        }
-        player4Plus.setOnClickListener {
-            player4Life++
-            player4LifeTextView.text = "$player4Life"
             // Call check for winner in case we need to clear the winning player textview
             checkForWinner()
         }
